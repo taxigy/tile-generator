623121ef0c8e8fcfbbd145fed618e52b083bbfc7
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-10-24 22:12:42 +0300

    core script is better

--- prepare.rb
+++ prepare.rb
@@ -1,25 +1,39 @@
-pdf = ARGV[0]
-ms = [[300, 16], [150, 15]]
+# replace ARGV with HTTP POST requires params
+source_images = ARGV[0]
+base_zoom = ARGV[1] or 16 # for example, 16
+source_format = ARGV[2] or "pdf" # "pdf", otherwise "jpg"
+base_density = ARGV[3] or 300 # base PDF density
+zoom_levels = ARGV[4] or [0, -1, -2]
 
-ms.each do |density, zoom|
-  png = "source_#{zoom}.png"
-  `convert -density #{density} -background white -alpha remove "#{pdf}" +append "#{png}"`
-  matching = /^.*PNG\s(\d+)x(\d+).*$/.match `identify #{png}`
-  width = matching[1].to_i
-  height = matching[2].to_i
-  width2 = width + 256 - (width % 256)
-  height2 = height + 256 - (height % 256)
-  cols = width2 / 256
-  rows = height2 / 256
-  puts "Initial dimensions: #{width}x#{height}, new dimensions: #{width2}x#{height2}"
-  `convert -gravity center -background white -extent "#{width2}x#{height2}" "#{png}" "#{png}"`
-  `convert -crop "256x256" +repage +adjoin "#{png}" "tile_#{zoom}_%01d.png"`
+dimensions = zoom_levels.map do |z|
+  density = (base_density * 2 ** z).to_i
+  scale = "#{(base_density * 2 ** z / base_density * 100).to_i}%"
+  [density, scale, z]
+end
+
+dimensions.each do |density, scale, zoom|
+  intermediary_png = "source_#{zoom}.png"
+  if source_format == "pdf"
+    `convert #{source_images.join(" ")} +append -resize #{scale} #{intermediary_png}`
+  else
+    `convert -density #{density} -background white -alpha remove "#{source_images}" +append "#{intermediary_png}"`
+
+  matching = /(\d+)x(\d+)$/.match `identify #{png}`
+  original_width = matching[1].to_i
+  original_height = matching[2].to_i
+  rounded_width = width + 256 - (width % 256)
+  rounded_height = height + 256 - (height % 256)
+  cols = (width2 / 256).to_i
+  rows = (height2 / 256).to_i
+  puts "Initial dimensions: #{original_width}x#{original_height}, new dimensions: #{rounded_width}x#{rounded_height}"
+  `convert -gravity center -background white -extent "#{rounded_width}x#{rounded_height}" "#{intermediary_png}" "#{intermediary_png}"`
+  `convert -crop "256x256" +repage +adjoin "#{intermediary_png}" "tile_#{zoom}_%01d.jpg"`
 
   col = 0
   row = 0
   (0...(cols * rows)).each do |n|
-    initial = "tile_#{zoom}_#{n}.png"
-    target = "tile_#{zoom}_#{col}_#{row}.png"
+    initial = "tile_#{zoom}_#{n}.jpg"
+    target = "tile_#{zoom}_#{col}_#{row}.jpg"
 
     puts "#{initial} -> #{target}"
 
