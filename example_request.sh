93994929932349066053b7313f8642c74bb7ce74
Alexey Badenkov <alexey.badenkov@gmail.com>
2016-10-24 22:49:20 +0300

    remove example file

--- example_request.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:3000 \
-     -H 'Content-Type: application/json' \
-     -d '{ "sources": [ "http://placehold.it/1000x1000", "http://placehold.it/1000x1000", "http://placehold.it/1000x1000" ], "offer": "abcxyz123"}'
