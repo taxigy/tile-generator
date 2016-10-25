5a15b78de92f4645e68f561dab895b207a4dd2d3
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-10-25 22:24:58 +0300

    filename fix

--- lib/cut.rb
+++ lib/cut.rb
@@ -66,7 +66,7 @@ def cut source_urls, offer
     `rm "#{intermediary_png}"`
 
     (0...images_count).each do |n|
-      `rm "#{offer}_#{n}.png"`
+      `rm "tile_#{offer}_#{n}.png"`
     end
   end
 end
