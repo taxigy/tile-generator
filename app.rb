1d3321c2f64856d63fb67030202262039c1cbc2c
Alexey Badenkov <alexey.badenkov@gmail.com>
2016-11-13 15:32:00 +0400

    Added validation

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/reloader'
 require 'sidekiq'
+require 'dry-validation'
+require 'uri'
 require_relative 'lib/tile_generator_worker'
+
+TileRequestSchema = Dry::Validation.Schema do
+  required('offer').filled
+  required('sources').filled(:array?) { each(:str?, format?: URI.regexp ) }
+end
 
 get "/" do
   "Welcome to Tile Generator"
@@ -10,7 +19,13 @@ post "/" do
   content_type :json
   req = JSON.load(request.body.read.to_s)
 
-  TileGeneratorWorker.perform_async(req['sources'], req['offer'])
+  validation = TileRequestSchema.call(req)
+  if validation.success?
+    TileGeneratorWorker.perform_async(req['sources'], req['offer'])
 
-  status(202)
+    status(202)
+  else
+    status 400
+    json errors: validation.messages
+  end
 end
